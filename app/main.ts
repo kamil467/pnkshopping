df0c9be65509d385b14be91dc09c489b284ba47c
kamil467 <kamil467@gmail.com>
2020-12-05 21:57:45 +0530

    Jasmine configuration added

--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,43 @@
+import '../global-jasmine'
+import 'jasmine-core/lib/jasmine-core/jasmine-html.js';
+import 'jasmine-core/lib/jasmine-core/boot.js';
+
 import './polyfills';
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
+import 'zone.js/dist/long-stack-trace-zone';
+import 'zone.js/dist/proxy.js';
+import 'zone.js/dist/sync-test';
 
-import { AppModule } from './app.module';
+// Requires 'zone.js/dist/proxy.js' and 'zone.js/dist/sync-test';
+import 'zone.js/dist/jasmine-patch';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+ 
+// stuff to test
+import '../app/app.component.spec.ts'
+
+jasmine.getEnv().configure({random: false});
+bootstrap();
+
+function bootstrap () {
+  if (window.jasmineRef) {
+    location.reload();
+    return;
+  } else {
+    window.onload();
+    window.jasmineRef = jasmine.getEnv();
   }
-  window['ngRef'] = ref;
 
-  // Otherise, log the boot error
-}).catch(err => console.error(err));
+  // First, initialize the Angular testing environment.
+  getTestBed().initTestEnvironment(
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting()
+  );
+}
